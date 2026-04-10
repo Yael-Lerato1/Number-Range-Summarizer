95635a3a16b35d33e6f4abe72e38070ec66ad0b3
Yael-Lerato1 <yaelerato@gmail.com>
2026-04-10 08:15:42 +0200

    Assumptions stated

---
+++ numberrangesummarizer/NumberRangeSummarizerImpl.java
@@ -0,0 +1,22 @@
+package numberrangesummarizer;
+
+import java.util.*;
+
+/** 
+ * @author Yael Lerato Kallmann
+ * 
+ * Implement this Interface to produce a comma delimited list of numbers, grouping the numbers into a range when they are sequential.
+ * 
+ * Assumptions:
+ * - null or blank input returns an empty collection
+ * - whitespace around numbers is trimmed
+ * - duplicates are removed
+ * - result is sorted ascending
+ * - single consecutive pair (e.g. 1,2) is still a range: "1-2"
+*/
+
+public class NumberRangeSummarizerImpl implements NumberRangeSummarizer {
+
+ 
+
+}
